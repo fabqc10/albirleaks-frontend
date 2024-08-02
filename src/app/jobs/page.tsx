9a039e8c0e9026149b62ea1b4bdf6f58aab3fc33
Fabio Quintero <fabio.quintero.cardona@gmail.com>
2024-08-02 16:45:50 +0200

    4 cards per row

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -5,11 +5,20 @@ import { useContext } from "react";
 import JobCard from "../components/jobCard";
 
 const Page = () => {
-const {jobs} = useContext(JobsContext);
-  return <div>
-    {jobs.map((job) => (
-        <JobCard job={job}/>
-    ))}
-  </div>;
+  const { jobs } = useContext(JobsContext);
+  return (
+    <div className="container mx-auto p-4">
+      <div className="flex flex-wrap -mx-2">
+        {jobs.map((job) => (
+          <div
+            key={job.jobId}
+            className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2"
+          >
+            <JobCard job={job} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Page;
