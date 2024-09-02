932622b75765ead3fc0fc4c1c7cde3f1ce3dd8be
Fabio Quintero <fabio.quintero.cardona@gmail.com>
2024-09-02 18:16:36 +0200

    updated banner

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -3,14 +3,23 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="w-full h-96 back bg-gray-300">
-      {/* <Image
-        src={"/assets/banner.png"}
-        alt="albir-logo"
-        layout="fill"
-        quality={100}
-        objectFit="contain"
-      /> */}
+    <div className="w-full h-96 bg-gray-300 flex items-center">
+      <div className="w-1/2 p-8">
+        <span>
+          Whether you need help moving, delivering, shopping, or recycling, our app
+          instantly connects you with thousands of helpers—everyday people with a
+          car and the time to help you out.
+        </span>
+      </div>
+      <div className="relative w-1/2 h-full">
+        <Image
+          src={"https://a.storyblok.com/f/198708/1037x875/4e391d1794/advertiser-with-stuff.png/m/460x0"}
+          alt="albir-logo"
+          layout="fill"
+          quality={100}
+          objectFit="contain"
+        />
+      </div>
     </div>
   );
 };
