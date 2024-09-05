c4612667522c847b43f4376f8e751b75ba7d053b
Fabio Quintero <fabio.quintero.cardona@gmail.com>
2024-09-05 19:50:19 +0200

    cambio titulo al español

--- src/app/components/banner2.tsx
+++ src/app/components/banner2.tsx
@@ -11,7 +11,7 @@ const Banner = () => {
       }}>
         <div className="absolute inset-0 bg-black opacity-40 z-10"></div>
         <div className="text-center z-20">
-          <h1 className="text-white text-6xl font-bold mb-4 shadow-md">A sustainable platform</h1>
+          <h1 className="text-white text-6xl font-bold mb-4 shadow-md">Una plataforma sostenible</h1>
           <p className="text-white text-2xl shadow-md">Find out how we started, what drives us, and how we’re reimagining cities moving.</p>
         </div>
       </div>
