9f11b6a5403740076151e40dc6e72b43e154b30c
Fabio Quintero <fabio.quintero.cardona@gmail.com>
2024-09-23 16:48:49 +0200

    fix in banner 2

--- src/app/components/banner2.tsx
+++ src/app/components/banner2.tsx
@@ -12,7 +12,7 @@ const Banner = () => {
         <div className="absolute inset-0 bg-black opacity-40 z-10"></div>
         <div className="text-center z-20">
           <h1 className="text-white text-6xl font-bold mb-4 shadow-md">Una plataforma sostenible</h1>
-          <p className="text-white text-2xl shadow-md">Find out how we started, what drives us, and how we’re reimagining cities moving.</p>
+          <p className="text-white text-2xl shadow-md">Que ofrece rapida ayuda , colaboración y trabajo.</p>
         </div>
       </div>
     );
